67a089b6f7224529ee5ce412d6f6a6d1075957e2
Jason Merrill <jwmerrill@gmail.com>
2018-12-19 14:54:46 -0800

    Remove unnecessary config

--- README.md
+++ README.md
@@ -6,11 +6,9 @@ Attempt to bundle the Desmos API using Webpack
 
 1. Install [NodeJS v8.X (LTS)](https://nodejs.org/en/download/)
 1. Copy a version of `calculator.js` into the `vendor/` folder
-1. Generate a copy of calculator.js with references to window.Desmos removed
-and place it in the src directory using `sed 's/window.Desmos/Desmos/g' < vendor/calculator.js > src/calculator.js`
 1. At command prompt, run:
     ```cmd
     npm install
-    npm run start
+    npm start
     ```
 1. Browser should open at http://localhost:8085
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,9 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require('webpack');
 
 module.exports = {
     mode: "development",
     devtool: "source-map",
     entry: `${__dirname}/src/entry.js`,
-    module: {
-        rules: [
-            {
-                test: path.resolve(__dirname, "src/calculator.js"),
-                use: [
-                    // Export the Desmos global var
-                    "exports-loader?Desmos",
-                    // Set module.exports=undefined and exports=undefined because Desmos has
-                    // dependencies that use UMD preludes that get confused when they are present.
-                    // Set the Desmos variable to an empty object, which will be used as the module export
-                    "imports-loader?undefined=>module.exports=undefined,exports=>undefined,Desmos=>{}"
-                ]
-            }
-        ]
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "src/template.html"
